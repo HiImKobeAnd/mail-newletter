2c056c1f0663217f990bd5799f0f359484f0b295
HiImKobeAnd <HiImKobeAnd@gmail.com>
2024-03-04 13:43:24 +0100

    Make port in tests random

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use mail_newletter::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Can't bind to port 8000");
+    run(listener)?.await
 }
