9fb5557ecd30aaf328490cedaf7ab9b357e2f8e3
HiImKobeAnd <HiImKobeAnd@gmail.com>
2024-03-11 11:46:23 +0100

    Add configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::net::TcpListener;
-
+use mail_newletter::configuration::get_configuration;
 use mail_newletter::startup::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Can't bind to port 8000");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener =
+        TcpListener::bind(address.clone()).expect(format!("Can't bind to {}", address).as_str());
     run(listener)?.await
 }
