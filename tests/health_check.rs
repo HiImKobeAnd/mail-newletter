8436098f2a26cb9f9bd60f9658408a0542109a8f
HiImKobeAnd <HiImKobeAnd@gmail.com>
2024-03-11 12:03:58 +0100

    Add database connection test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,5 @@
+use mail_newletter::configuration::{self, get_configuration};
+use sqlx::{Connection, PgConnection};
 use std::{net::TcpListener, vec};
 
 async fn spawn_app() -> String {
@@ -27,6 +29,12 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app().await;
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
