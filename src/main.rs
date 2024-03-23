61f678fa485479eeb66a816a4145812cc6fa7c70
HiImKobeAnd <HiImKobeAnd@gmail.com>
2024-03-23 21:17:03 +0100

    Change pgconnection to pgpool

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use mail_newletter::configuration::get_configuration;
 use mail_newletter::startup::run;
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener =
         TcpListener::bind(address.clone()).expect(format!("Can't bind to {}", address).as_str());
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
