2ff187a70a4ab9b77cfd00bb9466a27ef17fb9ce
HiImKobeAnd <HiImKobeAnd@gmail.com>
2024-06-27 13:53:30 +0200

    6.4 | guarantee SubscriberName to be valid through types

---
+++ src/domain.rs
@@ -0,0 +1,23 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+pub struct SubscriberName(String);
+
+pub struct NewSubscriber {
+    pub email: String,
+    pub name: SubscriberName,
+}
+
+impl SubscriberName {
+    pub fn parse(name: String) -> SubscriberName {
+        let is_empty_or_whitespace = name.trim().is_empty();
+        let is_to_long = name.graphemes(true).count() > 256;
+        let forbidden_charaters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = name.chars().any(|g| forbidden_charaters.contains(&g));
+
+        if is_empty_or_whitespace || is_to_long || contains_forbidden_characters {
+            panic!("{} is not a valid subscriber name.", name)
+        } else {
+            Self(name)
+        }
+    }
+}
