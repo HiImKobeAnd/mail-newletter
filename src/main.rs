b4c2575f670bc066f23b87c37324bf7c50340365
HiImKobeAnd <HiImKobeAnd@gmail.com>
2024-07-07 13:41:10 +0200

    refactor: :construction: Add reqwest client to app_data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use mail_newsletter::configuration::get_configuration;
+use mail_newsletter::email_client::EmailClient;
 use mail_newsletter::startup::run;
 use mail_newsletter::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
@@ -12,11 +13,14 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect_lazy_with(configuration.database.with_db());
 
+    let sender_email = configuration.email_client.sender().expect("Invalid sender email address.");
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener =
         TcpListener::bind(address.clone()).unwrap_or_else(|_| panic!("Can't bind to {}", address));
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
