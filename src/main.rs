10c32474a126aa58b46252b36253fdc720ec03ec
hiimkobeand <hiimkobeand@gmail.com>
2024-08-23 13:06:52 +0200

    refactor: seperate startup code from main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use mail_newsletter::configuration::get_configuration;
-use mail_newsletter::email_client::EmailClient;
-use mail_newsletter::startup::run;
+use mail_newsletter::startup::build;
 use mail_newsletter::telemetry::{get_subscriber, init_subscriber};
-use sqlx::PgPool;
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -11,26 +8,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy_with(configuration.database.with_db());
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener =
-        TcpListener::bind(address.clone()).unwrap_or_else(|_| panic!("Can't bind to {}", address));
-    run(listener, connection_pool, email_client)?.await
+    let server = build(configuration).await?;
+    server.await?;
+    Ok(())
 }
